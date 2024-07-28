d54b542acde6bde4ba1e4206cdf7885ec48bb592
nobregas <gabrielnobregasantos2@gmail.com>
2024-07-28 12:56:20 -0300

    add prod controler

--- src/routes/product.routes.ts
+++ src/routes/product.routes.ts
@@ -1,18 +1,18 @@
 import { Router } from "express";
 import { errorHandler } from "../error-handler";
-import { createProduct, deleteProduct, getProductById, listProduct, searchProducts, updateProduct } from "../controllers/product.controller";
 import authMiddleware from "../middlewares/auth.middleware";
 import adminMiddleware from "../middlewares/admin";
+import productController from "../controllers/product.controller";
 
 const productsRoutes: Router = Router();
 
-productsRoutes.post("/", [authMiddleware, adminMiddleware], errorHandler(createProduct));
-productsRoutes.delete("/:id", [authMiddleware, adminMiddleware], errorHandler(deleteProduct));
-productsRoutes.patch("/:id", [authMiddleware, adminMiddleware], errorHandler(updateProduct));
-productsRoutes.get("/:id", [authMiddleware, adminMiddleware], errorHandler(getProductById));
-productsRoutes.get("/", [authMiddleware, adminMiddleware], errorHandler(listProduct));
+productsRoutes.post("/", [authMiddleware, adminMiddleware], errorHandler(productController.createProduct));
+productsRoutes.delete("/:id", [authMiddleware, adminMiddleware], errorHandler(productController.deleteProduct));
+productsRoutes.patch("/:id", [authMiddleware, adminMiddleware], errorHandler(productController.updateProduct));
+productsRoutes.get("/:id", [authMiddleware, adminMiddleware], errorHandler(productController.getProductById));
+productsRoutes.get("/", [authMiddleware, adminMiddleware], errorHandler(productController.listProduct));
 
-productsRoutes.get("/search", [authMiddleware], errorHandler(searchProducts));
+productsRoutes.get("/search", [authMiddleware], errorHandler(productController.searchProducts));
 
 
 
