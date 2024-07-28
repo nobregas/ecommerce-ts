03d8de82a5e93b777b93ccffb336650fbf3be27c
nobregas <gabrielnobregasantos2@gmail.com>
2024-07-28 13:06:35 -0300

    refactor: add CartController class

--- src/routes/cart.routes.ts
+++ src/routes/cart.routes.ts
@@ -1,15 +1,14 @@
 import { Router } from "express";
 import authMiddleware from "../middlewares/auth.middleware";
 import { errorHandler } from "../error-handler";
-import { addItemToCart, changeQuantity, deleteItemFromCart, getCart } from "../controllers/cart.controller";
+import cartController from "../controllers/cart.controller";
 
 const cartRoutes: Router = Router();
 
-cartRoutes.post("/", [authMiddleware], errorHandler(addItemToCart))
-cartRoutes.get("/", [authMiddleware], errorHandler(getCart))
-cartRoutes.delete("/:id", [authMiddleware], errorHandler(deleteItemFromCart))
-cartRoutes.patch("/:id", [authMiddleware], errorHandler(changeQuantity))
-
+cartRoutes.post("/", [authMiddleware], errorHandler(cartController.addItemToCart))
+cartRoutes.get("/", [authMiddleware], errorHandler(cartController.getCart))
+cartRoutes.delete("/:id", [authMiddleware], errorHandler(cartController.deleteItemFromCart))
+cartRoutes.patch("/:id", [authMiddleware], errorHandler(cartController.changeQuantity))
 
 
 export default cartRoutes;
