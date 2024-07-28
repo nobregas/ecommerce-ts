0ab71a3eefa2b6d7b928ccea0a37859570a5416c
nobregas <gabrielnobregasantos2@gmail.com>
2024-07-28 13:04:56 -0300

    refactor: add AuthController class

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -10,43 +10,49 @@ import { UnProcessableEntity } from "../exceptions/validation";
 import { SignUpSchema } from "../schema/user.schema";
 import { NotFoundException } from "../exceptions/not-found";
 
-export const signup = async (req: Request, res: Response) => {
-    SignUpSchema.parse(req.body);
-        const {email, password, name} = req.body;
 
+
+class AuthController {
+    signup = async (req: Request, res: Response) => {
+        SignUpSchema.parse(req.body);
+            const {email, password, name} = req.body;
+    
+            let user = await prismaCLient.user.findFirst({where: {email}});
+        
+            if (user) throw new BadRequestException("User Already Exists", ErrorCodes.USER_ALREADY_EXISTS, null)
+            
+            user = await prismaCLient.user.create({
+                data:{
+                    name,
+                    email,
+                    password: hashSync(password, 10)
+                }
+            });
+            res.status(HttpStatus.CREATED).json(user);
+    }
+    
+    login = async (req: Request, res: Response) => {
+    
+        const {email, password} = req.body;
         let user = await prismaCLient.user.findFirst({where: {email}});
     
-        if (user) throw new BadRequestException("User Already Exists", ErrorCodes.USER_ALREADY_EXISTS, null)
+        if (!user) throw new NotFoundException("User not found", ErrorCodes.USER_NOT_FOUND)
+        if (!compareSync(password, user.password)) throw new BadRequestException("Incorrect Login", ErrorCodes.INCORRECT_LOGIN, null)
         
-        user = await prismaCLient.user.create({
-            data:{
-                name,
-                email,
-                password: hashSync(password, 10)
-            }
-        });
-        res.status(HttpStatus.CREATED).json(user);
-}
-
-export const login = async (req: Request, res: Response) => {
-
-    const {email, password} = req.body;
-    let user = await prismaCLient.user.findFirst({where: {email}});
-
-    if (!user) throw new NotFoundException("User not found", ErrorCodes.USER_NOT_FOUND)
-    if (!compareSync(password, user.password)) throw new BadRequestException("Incorrect Login", ErrorCodes.INCORRECT_LOGIN, null)
+        const token = jwt.sign({
+            userId: user.id
+        }, JWT_SECRET);
     
-    const token = jwt.sign({
-        userId: user.id
-    }, JWT_SECRET);
-
-    res.json({
-        user,
-        token
-    });
+        res.json({
+            user,
+            token
+        });
+    }
+    
+    me = async (req: Request, res: Response) => {
+    
+        res.json(req.user);
+    }
 }
 
-export const me = async (req: Request, res: Response) => {
-
-    res.json(req.user);
-}
+export default new AuthController();
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { login, me, signup } from "../controllers/auth.controller";
 import { errorHandler } from "../error-handler";
 import authMiddleware from "../middlewares/auth.middleware";
+import authController from "../controllers/auth.controller";
 
 const authRoutes: Router = Router();
 
-authRoutes.post("/signup", errorHandler(signup));
-authRoutes.post("/login", errorHandler(login));
-authRoutes.get("/me", [authMiddleware], errorHandler(me));
-
+authRoutes.post("/signup", errorHandler(authController.signup));
+authRoutes.post("/login", errorHandler(authController.login));
+authRoutes.get("/me", [authMiddleware], errorHandler(authController.me));
 
 export default authRoutes;
